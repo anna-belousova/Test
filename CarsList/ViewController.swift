efb0e8d69d3b3759359ef6b52331eb8edb84077d
Anna Belousova <anechka@style.ru>
2019-10-02 20:47:23 +0300

    Initial Commit

---
+++ CarsList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarsList
+//
+//  Created by MacBook Air on 02/10/2019.
+//  Copyright © 2019 MacBook Air. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
